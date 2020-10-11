44e9d5fad9939d3fcf69a6a0d5dfe601ad46e35b
sag <aitronics@system76-pc.localdomain>
2020-10-11 17:31:04 -0500

    Refactored MainActivity class

--- app/src/main/java/com/acme/sohunter/MainActivity.kt
+++ app/src/main/java/com/acme/sohunter/MainActivity.kt
@@ -16,69 +16,14 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var repository : Repository //TODO: Move this reference to the view model
     private lateinit var viewModel : SOViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        repository = Repository()
-        requestRecentQuestions()
+        viewModel = SOViewModel(Repository())
+        viewModel.requestRecentQuestions()
     }
 
-    private fun requestRecentQuestions() {
-        val request = repository.remoteSource()
-        val call = request.getRecentQuestions()
-
-        call.enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                Log.i("onResponse","Response: ${response.code()}")
-
-                if(response.isSuccessful) {
-                    val questions = response.body()
-                    Log.i("WE_DID_IT","Question Body: ${questions}")
-                }
-            }
-
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                Log.i("onFailure","Retrofit Network Error")
-            }
-
-        })
-    }
-
-    private fun callOAuth2Service(context: Context) {
-        val request = WebServiceBuilder.buildService(ApiService::class.java)
-        val call = request.oauthHtmlRequest()
-
-        call.enqueue(object : Callback<String> {
-
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                Log.i("onResponse","Auth Code: ${response.code()}")
-                if(response.isSuccessful) {
-                    val html = response.body()
-                    Log.i("WE_DID_IT","Auth Token: ${html}")
-
-                    val webView = findViewById<WebView>(R.id.web)
-                    webView.apply {
-                        settings.loadsImagesAutomatically = true
-                        settings.javaScriptEnabled = true
-                        settings.domStorageEnabled = true
-                        settings.useWideViewPort = true
-                        settings.loadWithOverviewMode = true
-                    }
-
-                    if (html != null) {
-                        webView.loadDataWithBaseURL(null,html,"text/html","utf-8",null)
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<String>, t: Throwable) {
-
-            }
-        })
-
-    }
 }
--- app/src/main/java/com/acme/sohunter/ui/SOViewModel.kt
+++ app/src/main/java/com/acme/sohunter/ui/SOViewModel.kt
@@ -1,10 +1,71 @@
 package com.acme.sohunter.ui
 
+import android.util.Log
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
+import com.acme.sohunter.R
+import com.acme.sohunter.data.api.ApiService
 import com.acme.sohunter.data.repository.Repository
+import com.acme.sohunter.utils.WebServiceBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SOViewModel(private val repository: Repository) : ViewModel() {
 
 
+     fun requestRecentQuestions() {
+        val request = repository.remoteSource()
+        val call = request.getRecentQuestions()
+
+        call.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.i("onResponse","Response: ${response.code()}")
+
+                if(response.isSuccessful) {
+                    val questions = response.body()
+                    Log.i("WE_DID_IT","Question Body: ${questions}")
+                }
+            }
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.i("onFailure","Retrofit Network Error")
+            }
+
+        })
+    }
+
+    private fun callOAuth2Service(context: AppCompatActivity) {
+        val request = WebServiceBuilder.buildService(ApiService::class.java)
+        val call = request.oauthHtmlRequest()
+
+        call.enqueue(object : Callback<String> {
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.i("onResponse","Auth Code: ${response.code()}")
+                if(response.isSuccessful) {
+                    val html = response.body()
+                    Log.i("WE_DID_IT","Auth Token: ${html}")
+
+                    val webView = context.findViewById<WebView>(R.id.web)
+                    webView.apply {
+                        settings.loadsImagesAutomatically = true
+                        settings.javaScriptEnabled = true
+                        settings.domStorageEnabled = true
+                        settings.useWideViewPort = true
+                        settings.loadWithOverviewMode = true
+                    }
+
+                    if (html != null) {
+                        webView.loadDataWithBaseURL(null,html,"text/html","utf-8",null)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+
+            }
+        })
+    }
 
 }
